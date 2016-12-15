1ce2e263f2a4892461a309454ebc8f2fb96ef3c2
Brett Minor <brett@Bretts-MacBook-Pro.local>
2016-12-14 23:25:26 -0500

    Working version

--- src/ContactManager.java
+++ src/ContactManager.java
@@ -1,5 +1,8 @@
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 import common.ContactInformation;;
 
@@ -9,8 +12,14 @@ public class ContactManager {
 	private static SessionFactory sessionFactory;
 	
 	static {
-		session = sessionFactory.openSession();
-	}
+			Configuration configuration = new Configuration();
+			configuration.configure("hibernate.cfg.xml");
+			 ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings( configuration.getProperties() ).build();
+			
+			sessionFactory = configuration.buildSessionFactory( serviceRegistry );
+			session = sessionFactory.openSession();
+			
+		} 
 	
 	public void displayEntries(){
 		
--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,7 @@ public class Main {
 	/* Static init block */
 	static{
 		scanner = new Scanner( System.in );
+		manager = new ContactManager();
 	}
 	
 	public static void main(String[] args) {
